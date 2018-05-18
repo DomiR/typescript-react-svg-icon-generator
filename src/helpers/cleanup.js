bb94b7be4cb6b87e480e56d0a8204048c3f93bf5
Dominique <domi.rau@gmail.com>
2018-05-18 15:07:26 +0200

    feat(clean): add stroke color and clean up a bit more

--- src/helpers/cleanup.js
+++ src/helpers/cleanup.js
@@ -4,13 +4,18 @@ module.exports.cleanupName = (name) => {
 
 module.exports.cleanupSvg = (svg) => {
     return svg
+    .replace(/ xmlns:xlink=".+?"/g, '')
+    .replace(/ xlink:href=".+?"/g, '')
     .replace(/ width="[^"]*"/, '')
     .replace(/ height="[^"]*"/, '')
     .replace(/ fill="#?\w+"/g, '')
     .replace(/ fill-rule="#?\w+"/g, '')
+    .replace(/ stroke="#?\w+"/g, '')
+    .replace(/ stroke-linecap="/g, ' strokeLinecap="')
+    .replace(/ stroke-linejoin="/g, ' strokeLinejoin="')
     .replace(/ clip-rule="#?\w+"/g, '')
     .replace(/\s{2,}/g, ' ')
     .replace(/ class="\w+"/g, '')
     .replace(/<style>.*<\/style>/g, match => match.replace(/<style>/g, '<style>{ `').replace(/<\/style>/g, '` }</style>'))
-    .replace(/viewBox/, 'fill={color} height={height || size} width={width || size} onClick={onClick} style={style} className={className} viewBox');
+    .replace(/viewBox/, 'fill={color} stroke={strokeColor} height={height || size} width={width || size} onClick={onClick} style={style} className={className} viewBox');
 };
